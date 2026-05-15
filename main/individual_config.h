03bf4e351634252de4155a27d80bef45faa0ac5f
ChopperJan <info@rodewald.eu>
2026-05-15 11:47:54 +0200

    Increase Display on delay on startup

--- main/individual_config.h
+++ main/individual_config.h
@@ -258,7 +258,7 @@
 #define EEZ_VALUE_FACTOR_2                 1000     ///< Scaling factor for float-to-integer conversion in LVGL
 #define EEZ_VALUE_FACTOR_3                 1000      ///< Scaling factor for float-to-integer conversion in LVGL
 #define EEZ_VALUE_FACTOR_4                 1000      ///< Scaling factor for float-to-integer conversion in LVGL
-#define GAUGE_ON_DELAY_MS                  1000      ///< Delay before gauges become active after boot (milliseconds)
+#define GAUGE_ON_DELAY_MS                  1200      ///< Delay before gauges become active after boot (milliseconds)
 #define BUZZER_ON_DELAY_MS                 5000      ///< Delay before buzzer activation is allowed (milliseconds)
 #define MAIN_TASK_FINISHED_DELAY           5000      ///< Delay for main task completion (milliseconds)
 
